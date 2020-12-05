d2724b3859b5e2119a2389dd1cd86fd5e0f3ba00
FrankAlbella <FrankAlbella@gmail.com>
2020-12-05 14:23:12 -0500

    Add ratings to Movie::print

--- src/movie.cpp
+++ src/movie.cpp
@@ -1,21 +1,30 @@
 #include "movie.hpp"
 #include <iostream>
 
-std::string Movie::getID()
+std::string Movie::getID() const
 {
     return this->name + "_" + this->year;
 }
 
-void Movie::print()
+void Movie::print() const
 {
     std::cout << this->name << std::endl;
     std::cout << '\t' << "Year: " << this->year << std::endl;
-    std::cout << '\t' << "Genres: ";
+    std::cout << '\t' << "Genres: " << vectorToString(this->genres) << std::endl;
+    std::cout << '\t' << "Avg. Rating: " << this->avgRating << std::endl;
+    std::cout << '\t' << "Number of Ratings: " << this->ratings << std::endl;
+}
+
+std::string Movie::vectorToString(const std::vector<std::string>& vect) const
+{
+    std::string str; 
+
     for(size_t i = 0; i < this->genres.size(); i++)
     {
-        std::cout << genres[i];
+        str += genres[i];
         if(i != this->genres.size()-1)
-            std::cout << ", ";
+            str += ", ";
     }
-    std::cout << std::endl;
+
+    return str;
 }
--- src/movie.hpp
+++ src/movie.hpp
@@ -5,19 +5,24 @@
 class Movie
 {
 private:
-
+    std::string vectorToString(const std::vector<std::string>& vect) const; 
 public:
     std::string movieId;
     std::string name;
     std::string year;
     std::string language;
-    int avgRating;
+    
+    float avgRating;
+    int ratings; 
+    
     std::vector<std::string> genres;
+    std::vector<std::string> directors;
+    std::vector<std::string> actors;
     int score = -1; 
 
-    std::vector<Movie> getRecommendations(std::map<std::string, Movie> movies);
-    void scoreMovies(std::map<std::string, Movie> movies);
-    void print();
+    std::vector<Movie> getRecommendations(std::map<std::string, Movie> movies) const;
+    void scoreMovies(std::map<std::string, Movie> movies) const;
+    void print() const;
     // Returns the ID used as the key to this movie in an std::map
-    std::string getID();
+    std::string getID() const;
 };
